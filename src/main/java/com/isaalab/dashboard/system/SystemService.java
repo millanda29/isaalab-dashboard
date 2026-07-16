9c4bb6d136b5adee416fb814992c4bc384d7a72f
Maikol Isaac Llanda Huatatoca <maikolllanda@gmail.com>
2026-07-15 20:57:44 -0500

    fix(backend): 🐛 add missing imports for List and Map in SystemService

--- src/main/java/com/isaalab/dashboard/system/SystemService.java
+++ src/main/java/com/isaalab/dashboard/system/SystemService.java
@@ -6,6 +6,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class SystemService {
